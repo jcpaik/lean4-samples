226ef47460bb73f60542fd2a305d8c3663fc0027
Jineon Baek <jineon@umich.edu>
2023-04-25 06:53:31 -0400

    version up natural numbers

--- NaturalNumbers/lakefile.lean
+++ NaturalNumbers/lakefile.lean
@@ -6,7 +6,7 @@ package naturalNumberGame {
 }
 
 require mathlib from git
-  "https://github.com/leanprover-community/mathlib4.git" @ "9efcb9508435caeb4281b14455f37b88f8ffc2e5"
+  "https://github.com/leanprover-community/mathlib4.git" @ "6137962d512b9f8c6c891858238cc5b6f8dba00b"
 
 
 lean_lib MyNat
